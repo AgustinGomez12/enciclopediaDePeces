44ff96cd61232a98769a65bc3a5199c05fec498d
AgustinGomez12 <assadog22@gmail.com>
2024-09-03 20:32:33 -0300

    reparacionVolverMenu

--- js/redireciones.js
+++ js/redireciones.js
@@ -3,10 +3,6 @@ const containerCasa = document.getElementById("containerCasa")
 //Variable de la casita
 
 //Funcio para redirigir a incio
-function volverInicio () {
-    containerCasa.addEventListener(`click`, ()=>{
-     window.location.href = "/index.html"
-    })
-}volverInicio()
+
 //Fucion para redirigir a inicio
 
