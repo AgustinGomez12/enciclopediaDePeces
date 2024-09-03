0fe91a3acf55fab394da2b23acc683f3f61b98c9
AgustinGomez12 <assadog22@gmail.com>
2024-09-02 21:26:51 -0300

    contenidoNoticias1

---
+++ js/redireciones.js
@@ -0,0 +1,12 @@
+//Variable de la casita 
+const containerCasa = document.getElementById("containerCasa")
+//Variable de la casita
+
+//Funcio para redirigir a incio
+function volverInicio () {
+    containerCasa.addEventListener(`click`, ()=>{
+     window.location.href = "/index.html"
+    })
+}volverInicio()
+//Fucion para redirigir a inicio
+
